37ca0dba093056284c91891121542b56a2066ce3
isaali_Nurbak <isaali032@gmail.com>
2024-08-25 16:26:16 +0500

    init

---
+++ android/app/src/main/kotlin/com/example/kaspi_pdf_reader/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kaspi_pdf_reader
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:kaspi_pdf_reader/app/app.dart';
+
+void main() {
+
+  
+  runApp(const App());
+}
